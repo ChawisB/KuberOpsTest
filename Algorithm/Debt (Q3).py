4cf6a355c31f2e3fef43895b0ed3be2f6cf63ae6
Chawis B <26735251+ChawisB@users.noreply.github.com>
2021-11-15 22:52:36 +0700

    Create Debt (Q3).py

---
+++ Algorithm/Debt (Q3).py
@@ -0,0 +1,7 @@
+#Very confused with the question but due to time running out couldn't ask so judging from the question I assumed it's a straight up print
+def splitTheBill(n):
+    print(n)
+
+group = {"A": 20, "B": 15, "C": 10}
+
+splitTheBill(group)
