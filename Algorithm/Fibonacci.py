1487b51012489b1b82bba7fb880ff80ab41d0828
Chawis B <26735251+ChawisB@users.noreply.github.com>
2021-11-15 11:12:04 +0700

    Update Fibonacci.py

--- Algorithm/Fibonacci.py
+++ Algorithm/Fibonacci.py
@@ -1,12 +1,12 @@
 def fibonacci(n):
-    if n>0:
+    if n>0: #Checks if number is more than 0
         if n==1:
             return 0
         elif n==2:
             return 1
         else:
             return (fibonacci(n-1) + fibonacci(n-2))
-    else:
+    else: #Just in case 0 or negative input is given
         return "Invalid"
 
 print(fibonacci(9))
